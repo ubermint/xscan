a05cb870ffa6ddde590197afde3b669c3b339e81
Mint <vadim13@gmail.com>
2022-05-29 16:45:23 +0300

    xscan v0.3

--- xscan.py
+++ xscan.py
@@ -6,65 +6,84 @@ import socket
 import ipaddress
 import threading
 import subprocess
+import argparse
 import errno
 import time
 
+from alive_progress import alive_bar
 
-class Scanner():
-    def __init__(self, net, ports):
-        self.net = net
-        self.ports = ports
-
-        if self.net.prefixlen < 31:
-            self.hosts = self.net.num_addresses - 2
-        else:
-            self.hosts = self.net.num_addresses
-
-    def scan(self):
-        active = 0
-        print(f"Scanning top{len(self.ports)} TCP ports for {self.hosts} hosts.")
-        for addr in self.net.hosts():
-            ip = str(addr)
-            host = Host(ip)
-            if host.ICMP_ping():
-                active += 1
-                output = host.scan_ports(self.ports)
-                if output:
-                    for port in output:
-                        try:
-                            name = socket.getservbyport(port[0])
-                        except:
-                            name = "unknown"
-                        print(f"\tPORT {port[0]}/tcp {name} - {port[1]}")
-                else:
-                    print("\tNo open ports detected.")
-
-        print(f"Scan complete! Detected {active} active hosts.")
+class Database():
+    def __init__(self, report):
+        self.report = report
+    
+    def load(self):
+        pass
 
     def report(self):
         pass
 
-
-class Host():
-    def __init__(self, addr, delay=5):
-        self.addr = addr
+class Scanner():
+    def __init__(self, network, top_n_ports, delay):
+        self.network = network
         self.delay = delay
+        with open("tcp_ports.txt", "r") as f:
+            self.ports = list(map(int, f.read().strip().split(",")))[:top_n_ports]
+        
 
-    def scan_ports(self, ports):
+    def run(self):
+        active = 0
+        num_hosts = self.network.num_addresses - 2 if self.network.prefixlen < 31 else self.network.num_addresses
+        print(f"Scanning top{len(self.ports)} TCP ports for {num_hosts} hosts.")
+
+        result = {}
+        with alive_bar(num_hosts, enrich_print=False) as bar:
+            for addr in self.network.hosts():
+                host = Host(str(addr), self.delay)
+                latency = host.ICMP_ping()
+
+                if latency:
+                    print(f"Host {host.addr} is up! Latency {latency} ms.")
+                    active += 1
+                    output = self.scan_ports(host)
+                    if output:
+                        for port in output:
+                            try:
+                                name = socket.getservbyport(port[0])
+                            except:
+                                name = "unknown"
+                            print(f"\tPORT {port[0]}/tcp {name} - {port[1]}")
+                    else:
+                        print("\tNo open ports detected.")
+
+                bar()
+
+            print(f"Scan completed! Detected {active} active hosts.")
+
+
+    def scan_ports(self, host):
         threads = []
         output = []
 
-        for port in range(len(ports)):
-            t = threading.Thread(target=self.TCP_connect, args=(ports[port], output))
+        for port in range(len(self.ports)):
+            t = threading.Thread(target=host.TCP_connect, args=(self.ports[port], output))
             threads.append(t)
 
-        for i in range(len(ports)):
+        for i in range(len(self.ports)):
             threads[i].start()
 
-        for i in range(len(ports)):
+        for i in range(len(self.ports)):
             threads[i].join()
 
         return output
+
+    def report(self):
+        print("")
+
+
+class Host():
+    def __init__(self, addr, delay):
+        self.addr = addr
+        self.delay = delay
 
     def TCP_connect(self, port, output):
         TCPsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -78,6 +97,7 @@ class Host():
             #output.append((port, "filtered"))  # unable to distinguish timeout from firewall filtering
         except socket.error as err:
             pass
+        TCPsock.close()
 
     def ICMP_ping(self):
         "ping -c 1 -W 0.5 ip > /dev/null 2>&1"
@@ -85,36 +105,52 @@ class Host():
         proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
         output = str(proc.communicate()[0])
         if proc.returncode == 0:
-            latency = float(output[output.index("time")+5:output.index("ms")])
-            print(f"Host {self.addr} is up! Latency {latency} ms.")
-        return proc.returncode == 0
+            return float(output[output.index("time")+5:output.index("ms")])
+        return None
 
 
 def main():
-    if len(sys.argv) >= 2:
-        arg = sys.argv[1]
-        if "/" not in arg:
-            arg += "/32"
-        net = ipaddress.ip_network(arg, strict=False)
-        if not net.is_private:
-            sys.exit("\nOnly for private subnets!")
+    print("xscan v0.3")
 
-        if len(sys.argv) == 3 and (1 <= int(sys.argv[2]) <= 1000):
-            n_port = int(sys.argv[2])
+    parser = argparse.ArgumentParser()
+    parser.add_argument("network", help="Private subnetwork for scan.")
+    parser.add_argument("-d", "--delay", type=int, help="Delay in seconds. (default: 5s)")
+    parser.add_argument("-t", "--top", type=int, help="Number of most used ports. (default: 100)")
+    parser.add_argument("-r", "--report",
+        action="store_true", help="Report based on report.json (default: off)")
+
+    args = parser.parse_args()
+
+    try:
+        network = ipaddress.ip_network(args.network, strict=False)
+        if not network.is_private:
+            sys.exit("Error! Only for private network.")
+    except:
+        sys.exit("Error! Invalid network.")
+
+
+    delay = 3
+    if args.delay:
+        if (1 <= args.delay <= 10):
+            delay = args.delay
         else:
-            n_port = 100
+            sys.exit("Error! Invalid delay.")
 
-        with open("top.txt", "r") as f:
-            ports = list(map(int, f.read().strip().split(",")))[:n_port]
+    top_n_ports = 100
+    if args.top:
+        if 1 <= args.top <= 1000:
+            top_n_ports = args.top
+        else:
+            sys.exit("Error! Invalid number of ports.")
 
-        scanner = Scanner(net, ports)
-        try:
-            scanner.scan()
-        except KeyboardInterrupt:
-            sys.exit("\nScanner aborted.")
+    scanner = Scanner(network, top_n_ports, delay)
 
-    else:
-        print("Usage: xscan [network]{/[mask]} {ports}")
+    try:
+        result = scanner.run()
+    except KeyboardInterrupt:
+        sys.exit("\nScanner aborted by interruption.")
+
+
 
 if __name__ == "__main__":
     main()
